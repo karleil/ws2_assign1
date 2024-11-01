b6304326679d13cb784541ecdad5c4bb9adad37e
karleil <karleillvillareal@gmail.com>
2024-10-31 21:30:47 -0700

    FINAL FINAL

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # js_assign2
 
 Assignment for Web Scripting 2
+GITHUB LINK :
 
 Third Party Packages used:
 
@@ -8,3 +9,17 @@ Tailwind CSS: npm install tailwindcss@latest postcss@latest autoprefixer@latest 
 npx tailwind init -p
 
 IonIcons: npm install @reacticons/ionicons --save
+
+To run the code, open terminal by using these commands from WS2_ASSIGN1:
+cd guitar_store
+npm install
+npm run dev
+
+The Process of Creating the Website:
+
+- I started by creating a React project from an empty folder by using the command npx create-vite@latest guitar-store --template react
+- I then installed the Tailwind CSS and IonIcons packages by running the commands, npm install tailwindcss@latest postcss@latest autoprefixer@latest --save
+  npx tailwind init -p and npm install @reacticons/ionicons --save
+- After checking that everything works, I started making the three most easiest components Header, Banner, and Footer.
+- For the first three components, I find it easy because there are no deep interactivity needed just the hover animation.
+- The most challenging part was the Products component. To do this, I had to check the week 5 folder for references.
--- guitar_store/src/Components/Header.jsx
+++ guitar_store/src/Components/Header.jsx
@@ -1,36 +1,23 @@
-import React, { useState } from "react"; // Imports React and the useState hook for managing component state
-import Guitar from "./Guitar.jsx"; // Imports the Guitar component to render each individual guitar item
-import guitarsData from "../guitars.json"; // Imports guitar data from our external JSON file guitars.json
+import React from "react";
 
-function Products() { 
-  const [guitar, setGuitars] = useState(guitarsData); // Initializes guitar state with data from guitarsData
-  const [favs, setFavs] = useState([]); // Initializes favs state to keep track of favorite guitar IDs
 
-  // This function is needed to handle adding and removing a guitar from favorites
-  const handleFavClick = (guitarID) => {
-    if (favs.includes(guitarID)) { // If else statement to seeif the guitar ID is already in favorites
-      const newFavs = favs.filter((singleFav) => guitarID !== singleFav); // If so, filter it out of favs
-      setFavs(newFavs); // Updates favorites state without the removed ID
-    } else {
-      setFavs([...favs, guitarID]); // If not in favorites, add the guitar ID to favs
-    }
-  };
+function Header () {
+    return (
+     <header className="bg-black">
+        <nav className="flex justify-between items-center  w-[80%] mx-auto py-4">
+                <img src="../logo.svg" alt="logo" className="w-36"/>
+            <div className="flex item-center">
+                 <li><a className="text-white hover:text-yellow-500 px-4 invisible lg:visible" href="#">Home</a></li>
+                 <li><a className="text-white hover:text-yellow-500 px-4 invisible lg:visible" href="#">Services</a></li>
+                 <li><a className="text-white hover:text-yellow-500 px-4 invisible lg:visible" href="#">About</a></li>
+                 <li><a className="text-white hover:text-yellow-500 px-4 invisible lg:visible" href="#">Contact</a></li>
+            </div>
+            <div > 
+                <button className="font-semibold bg-yellow-500 hover:bg-white px-4 py-1 rounded-lg  text-black hover:text-yellow-500 invisible md:visible lg:visible">Sign In</button>
+            </div>
+        </nav>
+    </header>
+    );
+};
 
-  return (
-    <div className="flex flex-col items-center"> 
-      <h1 className="text-4xl font-bold text-center text-black mb-8">Products</h1> 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center"> 
-        {guitar.map((singleGuitar) => ( // Map over guitar array to render each item
-          <Guitar
-            key={singleGuitar.id} // Unique key for each guitar item
-            guitar={singleGuitar} // Passes guitar data to the Guitar component as a prop
-            favourites={favs} // Passes the favorites array to check if this guitar is a favorite
-            handleFavClick={handleFavClick} // Passes the handleFavClick function to manage favorites
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default Products; // Exports the Products component to be used in other pages
+export default Header;
